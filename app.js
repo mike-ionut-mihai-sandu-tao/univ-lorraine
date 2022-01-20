d328f5ebbff521326f2b37bbe3778945ec04ed28
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2022-01-20 10:27:25 +0100

    Running message

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 const port = 9999;
 
 
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-
 var express = require('express');
 var app = express();
 
@@ -27,4 +12,6 @@ app.get('/', function(req, res){
   }
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`nicoapp listening at http://localhost:${port} please wash your hands after use.`)
+})
