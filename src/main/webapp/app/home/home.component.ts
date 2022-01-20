98de566f4e734e3140ce8232b1bb5add425675f1
Clément <clement.mercier@hotmail.com>
2022-01-20 11:00:45 +0100

    ✨Hello World

---
+++ src/main/webapp/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
+
+@Component({
+  selector: 'jhi-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  account: Account | null = null;
+  public name: string | null = '';
+
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private accountService: AccountService, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+    this.name = this.route.snapshot.queryParamMap.get('name') ? this.route.snapshot.queryParamMap.get('name') : 'Anonymous';
+  }
+
+  login(): void {
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
