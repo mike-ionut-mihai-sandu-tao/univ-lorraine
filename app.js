c8b25054c0002fb585273bba2cb4b1cf4bcbe2e1
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2022-01-20 10:23:22 +0100

    Patch hello student

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
 const port = 9999;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  if(typeof req.query.id == "undefined"){
+    res.send('Hello world');
+  } else {
+  res.send('Hello ' + req.query.id);
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
